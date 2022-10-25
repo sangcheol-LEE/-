b2cf83661e290949e97e8c6cdc1b3b5775d9fe13
이상철 <zlrz6706@gmail.com>
2022-10-25 23:47:52 +0900

    연결리스트 복습

---
+++ 연결리스트.mjs
@@ -0,0 +1,49 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  // 연결리스트의 모든 원소를 출력하는 함수
+  printAll() {
+    let currentNode = this.head;
+
+    while(currentNode !== null){
+      console.log(currentNode.data)
+      currentNode = currentNode.next
+    }
+  }
+
+  // 원하는 인덱스에 데이터를 삽입하는 함수
+  insertAt(index, data) {
+    if(index > this.count || index < 0) {
+      throw new Error("범위를 벗어났습니다.")
+    }
+
+    let newNode = new Node(data);
+
+    if(index == 0) {
+      newNode.next = this.head;
+      this.head = newNode
+    } else {
+      // 원하는 인덱스까지 노드를 타고 들어가서 새로 삽입하는 경우
+      let currentNode = this.head;
+
+      for(let i =0; i < index - 1; i++ ) {
+        currentNode = currentNode.next
+      }
+      newNode.next = currentNode.next
+      currentNode.next = newNode
+    }
+    this.count++
+  }
+}
+
+export { Node, LinkedList }
