a11862d863078028b1e4b8d8fcf5aa77cfa4b46c
이상철 <zlrz6706@gmail.com>
2022-11-11 23:53:00 +0900

    선택정렬

---
+++ 자료구조_복습/SelectionSort.mjs
@@ -0,0 +1,30 @@
+/*
+  1. 선택 정렬은 배열의 정렬되지 않은 영역의 첫 번째 원소를 시작으로 마지막 원소까지 비교 후 가장 작은 값을 첫 번째 원소로 가져옵니다.
+  2. 그 다음도 정렬되지 않은 영역의 첫 번째 원소를 시작으로 마지막 원소까지 비교 후 가장 작은 값을 첫 번째 원소로 가져옵니다.
+
+*/
+
+
+const SelectionSolt = (arr) => {
+  for(let i = 0; i < arr.length - 1; i++) {
+    let minimumIndex = i
+
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[j] < arr[minimumIndex]) {
+        minimumIndex = j
+      }
+    }
+
+    let temp = arr[i];
+    arr[i] = arr[minimumIndex]
+    arr[minimumIndex] = temp
+  }
+}
+
+const x = [3,2,5,1,23,31,6,8];
+console.log(x)
+console.log("===== before sort ======")
+SelectionSolt(x)
+console.log(x)
+
+
