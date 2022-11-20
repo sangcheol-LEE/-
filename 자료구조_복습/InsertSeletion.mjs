c865ed5a760a9252c951c490710c553429a96fe2
이상철 <zlrz6706@gmail.com>
2022-11-21 06:53:20 +0900

    선택정렬 복습

--- 자료구조_복습/InsertSeletion.mjs
+++ 자료구조_복습/InsertSeletion.mjs
@@ -1,31 +1,29 @@
-const InsetionSort = (arr) => {
+const insert = arr => {
 
   for(let i = 1; i < arr.length; i++) {
-    console.log("i",i)
-    let insertingData = arr[i];
+    let 정렬되지_않은_함수 = arr[i];
     let j;
 
-      for(j = i - 1; j >= 0; j--) {
-        console.log("j",j)
-        if(arr[j] > insertingData) {
-          arr[j + 1] = arr[j]
-        } else {
-          break;
-        }
+    for(j = i - 1; j >= 0; j--) {
+      if(arr[j] > 정렬되지_않은_함수) {
+        arr[j + 1] = arr[j]
+      }else {
+        break;
       }
-
-    arr[j + 1] = insertingData;
+    }
+    arr[j + 1] = 정렬되지_않은_함수
   }
 
 }
 
-const x = [3,5,4,1,667,65,2423,1,99,7,8,776]
 
-console.log("==== 정렬 전 ====")
+const x = [6,3,16,9,1]
+
+console.log("정렬 전")
 console.log(x)
 
-InsetionSort(x)
+insert(x)
 
-console.log("==== 정렬 후 ====")
+console.log("정렬 후")
 console.log(x)
 
